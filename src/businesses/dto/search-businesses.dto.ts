d1b95e900ec0fcf3409fe3849e9baeb8e6e5c498
NimaAhmadi <mrnima2920@gmail.com>
2026-08-15 15:42:12 +0330

    feat(dto): add IsDateString import for upcoming since filter

--- src/businesses/dto/search-businesses.dto.ts
+++ src/businesses/dto/search-businesses.dto.ts
@@ -1,4 +1,4 @@
-import { IsOptional, IsString, IsInt, Min, Max, IsUUID, Matches } from 'class-validator';
+import { IsOptional, IsString, IsInt, Min, Max, IsUUID, Matches, IsDateString } from 'class-validator';
 import { Type } from 'class-transformer';
 import { ApiPropertyOptional } from '@nestjs/swagger';
 
